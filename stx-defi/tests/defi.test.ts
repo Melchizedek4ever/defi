94fda61ec2a5fd6671b7fb0e2e66590b89e1aa5c
Ezra Kewa <88113467+Melchizedek4ever@users.noreply.github.com>
2024-12-12 15:04:13 +0100

    Update defi.test.ts

--- stx-defi/tests/defi.test.ts
+++ stx-defi/tests/defi.test.ts
@@ -1,3 +1,4 @@
+import { initSimnet } from '@hirosystems/clarinet-sdk';
 import { describe, it, expect } from 'vitest';
 import { Cl } from '@stacks/transactions';
 
@@ -9,5 +10,17 @@ describe('stx-defi', () => {
     const amount = 1000;
     const deposit = simnet.callPublicFn('defi', 'deposit', [Cl.uint(amount)], wallet1);
     expect(deposit.result).toBeOk(Cl.bool(true));
+
+    const totalDeposits = simnet.getDataVar('defi', 'total-deposits');
+    expect(totalDeposits).toBeUint(amount);
+
+    const balance = simnet.callReadOnlyFn('defi', 'get-balance-by-sender', [], wallet1);
+    expect(balance.result).toBeOk(
+      Cl.some(
+        Cl.tuple({
+          amount: Cl.uint(amount),
+        })
+      )
+    );
   });
 });
